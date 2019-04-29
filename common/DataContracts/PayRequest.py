d6ef6bab3dab3b885bd74136cd002ffbc9f52786
Maxim Tolgobolskiy <maxim.tolgobolskiy@valant.com>
2019-04-29 15:23:44 +0300

    Fix pr comments

--- common/DataContracts/PayRequest.py
+++
@@ -1,9 +0,0 @@
-class PayRequest:
-    id = ""
-    address = ""
-    amount = 0
-
-    def __init__(self, id, address, amount):
-        self.id = id
-        self.address = address
-        self.amount = amount
--- transaction-optimizer/UtxoService.py
+++ transaction-optimizer/UtxoService.py
@@ -7,24 +7,6 @@ Licensed under the Apache License, Version 2.0 (the "License"); you may not use 
 Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the License for the specific language governing permissions and limitations under the License.
 """
 
-"""
-UTXO response
-  {
-    "txid" : "txid",          (string) the transaction id
-    "vout" : n,               (numeric) the vout value
-    "address" : "address",    (string) the bitcoin address
-    "account" : "account",    (string) DEPRECATED. The associated account, or "" for the default account
-    "scriptPubKey" : "key",   (string) the script key
-    "amount" : x.xxx,         (numeric) the transaction output amount in BTC
-    "confirmations" : n,      (numeric) The number of confirmations
-    "redeemScript" : n        (string) The redeemScript if scriptPubKey is P2SH
-    "spendable" : xxx,        (bool) Whether we have the private keys to spend this output
-    "solvable" : xxx,         (bool) Whether we know how to spend this output, ignoring the lack of keys
-    "safe" : xxx              (bool) Whether this output is considered safe to spend. Unconfirmed transactions
-                              from outside keys and unconfirmed replacement transactions are considered unsafe
-                              and are not eligible for spending by fundrawtransaction and sendtoaddress.
-  }
-"""
 class UtxoService:
   "monitors Unspent Transaction Outputs"
   
